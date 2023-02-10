963c3b3b3ed87678845616f09b79354309bb87d4
krishmk1009 <krushnamk1009@gmail.com>
2023-02-10 13:32:43 +0530

    login auth commited

--- Router/auth.js
+++ Router/auth.js
@@ -7,8 +7,8 @@ router.post('/register', async (req, res) => {
 
     try {
         //generate password using hash
-        const salt =await bcrypt.genSalt(10);
-        const hashedPassword =await bcrypt.hash(req.body.password, salt)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
 
         //create user
         const newUser = new User({
@@ -24,6 +24,35 @@ router.post('/register', async (req, res) => {
     catch (err) {
         console.log(err)
     }
+})
+
+//login
+/*
+Json:
+{
+   
+   "emailId": "demkmo@gmail",
+   "password": "mkmkmk"
+}
+
+*/
+
+router.post('/login', async (req, res) => {
+
+    try {
+        const user = await User.findOne({ emailId: req.body.emailId });
+        !user && res.status(404).json("user not found")
+
+        const validPassword = await bcrypt.compare(req.body.password, user.password)
+        !validPassword && res.status(400).json("not valid password")
+
+        res.status(200).json(user)
+
+    }
+    catch (err) {
+        console.log(err)
+    }
+
 })
 
 module.exports = router;
