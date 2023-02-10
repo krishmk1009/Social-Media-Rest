44dc32f089a0034fd3d5459139b45f18c2fcc0e1
krishmk1009 <krushnamk1009@gmail.com>
2023-02-10 13:15:11 +0530

    register commited

--- Router/auth.js
+++ Router/auth.js
@@ -1,14 +1,23 @@
 const router = require("express").Router();
 const User = require("../models/user")
+const bcrypt = require("bcrypt")
 
+//register
 router.post('/register', async (req, res) => {
-    const newUser = new User({
-        userName: req.body.userName,
-        emailId: req.body.emailId,
-        password: req.body.password,
-    })
 
     try {
+        //generate password using hash
+        const salt =await bcrypt.genSalt(10);
+        const hashedPassword =await bcrypt.hash(req.body.password, salt)
+
+        //create user
+        const newUser = new User({
+            userName: req.body.userName,
+            emailId: req.body.emailId,
+            password: hashedPassword,
+        })
+
+        //save user
         const user = await newUser.save();
         res.status(200).json(user);
     }
